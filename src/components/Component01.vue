0393e7176ce0dec41610a0f6230f136d6c3df908
liyingcold <2608953051@qq.com>
2019-10-14 15:53:57 +0800

    父子组件传值（通过获取dom)

--- src/components/Component01.vue
+++ src/components/Component01.vue
@@ -3,10 +3,13 @@
     <div id="home">
         <!-- 父组件向子组件传值
             1、为其动态绑定一个属性 -->
-        <v-header :title="title" :run="run" :home="this"></v-header>
+        <!--  <v-header :title="title" :run="run" :home="this"></v-header> -->
+        <v-header ref="header"></v-header>
         <hr>
         首页组件
-        <!-- <h2>这是第一个组件Component01---{{msg}}</h2>
+        <!-- 父组件获取子组件的数据和方法 -->
+        <button @click="getChildData()">获取子组件的数据和方法</button>
+        <!-- <h2>这是第一个组件Component01-{{msg}}</h2>
         <hr> 
         <v-life v-if="flag"></v-life>
         <button @click="flag=!flag">挂载，卸载life组件</button>
@@ -23,15 +26,24 @@
     export default{
         data() {
             return {
-                msg:'我是一个组件，挂载在根组件下',
+                msg:'我是一个总组件，挂载在根组件下',
                 title:'首页',
                 flag:true,
                 list:[]
             }
         },
         methods: {
-           run(data){
-               alert('我是父组件的run方法'+data)
+        //    run(data){
+        //        alert('我是父组件的run方法'+data)
+        //    },
+        run(){
+            alert('我是父组件的run方法');
+        },
+           getChildData(){
+               /*获取子组件的数据*/
+            //    alert(this.$refs.header.msg)
+            /*获取子组件的方法*/
+            this.$refs.header.run()
            }
         },
         components:{
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
 <template>
     <div id="header">
         <!-- <h2>这是一个头部组件  挂载在Component01下</h2> -->
-        <h5>{{title}}</h5>
-        <button @click="run('+子组件的值')">执行父组件的方法</button>
-        <button @click="getParent">获取父组件的数据和方法</button>
+        <h5>{{msg}} </h5>
+        <!-- <button @click="run('+子组件的值')">执行父组件的方法</button> -->
+        <!-- <button @click="getParent">获取父组件的数据和方法</button> -->
+         <button @click="getParentData()">获取父组件的数据和方法</button>
     </div>
 </template>
 
@@ -15,28 +16,45 @@
                 props:['title'] 表示接收父组件传来的title 
             !!不仅可以传数据，还可以传方法
             !!还可以传类
-     子组件向父组件传值
-            1、
+
+     父组件主动获取子组件的数据和方法
+            1、调用子组件的时候，定义一个ref <v-header ref="Header"></v-header>
+            2、在父组件里面通过
+                this.$refs.header.属性
+                this.$refs.header.方法
+
+     子组件主动获取父组件的数据和方法
+            1、this.$parent.header.数据
+            2、this.$parent.header.方法
+
+
 */
     export default{
         data() {
             return {
-                msg:'msg'
+                msg:'我是一个头部组件'
             }
         },
         methods: {
-            getParent(){
-               alert(this.home.title) 
+            // getParent(){
+            //    alert(this.home.title) 
+            // }
+            run(){
+                alert("我是一个子组件的run方法");
+            },
+            getParentData(){
+                
+                alert(this.$parent.msg)
             }
-        },
+        }
         /*父组件向子组件传值*/
         // props:['title','run','home']
         /*props验证*/
-        props:{
+        /*props:{
             "title":String,
             "run":Object,
             "home":Object
-        }
+        }*/
     }
 </script>
 
